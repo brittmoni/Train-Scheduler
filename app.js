f6f3e842a2c67d93e7ef4df1a0e7ecc5237df2a6
Brittany F <brittfranklin92@gmail.com>
2019-03-10 01:07:52 -0500

    Initializing Firebase

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: config.MY_KEY,
+  authDomain: "britt-project.firebaseapp.com",
+  databaseURL: "https://britt-project.firebaseio.com",
+  projectId: "britt-project",
+  storageBucket: "britt-project.appspot.com",
+  messagingSenderId: "143368044597"
+};
+firebase.initializeApp(config);
